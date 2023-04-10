12e4f594b8f392c22e23253828b5a9ed737ae3d3
MonikaDimitrovaa <126509858+MonikaDimitrova17@users.noreply.github.com>
2023-04-10 20:08:59 +0200

    added style components and styled the images a bit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function Popular() {
 const [popular, setPopular] = useState([]);
@@ -11,6 +12,7 @@ const getPopular = async () => {
   const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
   const data = await api.json();
   setPopular(data.recipes);
+  console.log(data.recipes);
 } 
 
 
@@ -20,14 +22,37 @@ const getPopular = async () => {
     <div>
 {popular.map((recipe) => {
   return(
-    <div key={recipe.id}>
-      <p>{recipe.title}</p>
-       </div>
+    <Wrapper>
+      <h3>Popular Picks</h3>
+      {popular.map((recipe) => {
+        return (
+         <Card>
+          <p>{recipe.title}</p>
+          <img src={recipe.image} alt={recipe.title} />
+         </Card>
+        );
+      })}
+    </Wrapper>
   );
 })}
           
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  
+  img{
+    border-radius: 2rem;
+  }
+
+`;
 
 export default Popular;
